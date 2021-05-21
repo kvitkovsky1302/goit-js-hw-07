77dbb706fc10b592f6d13bcbb15605f1358ede42
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-05-22 00:40:56 +0300

    фиксит проблему с пробелами в task-6

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const validationInput = document.querySelector('#validation-input');
 
 const onBlurInput = event => {
     validationInput.classList.add('invalid');
-    event.currentTarget.value.length === +validationInput.dataset.length
+    event.currentTarget.value.length === +validationInput.dataset.length && event.target.value.trim() !== ''
         ? validationInput.classList.replace('invalid', 'valid')
         : validationInput.classList.replace('valid', 'invalid')
 };
